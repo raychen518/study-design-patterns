0c04b354272a182ede0398481ae52d9355e6dec4
Ray Chen <raychen518@gmail.com>
2016-06-25 11:59:38 +0800

    Add one more Pizza example for the Factory design pattern.

---
+++ src/main/java/com/raychen518/study/designpatterns/creational/factory/examples/pizzas/ok2/ChicagoCheesePizza.java
@@ -0,0 +1,4 @@
+package com.raychen518.study.designpatterns.creational.factory.examples.pizzas.ok2;
+
+public class ChicagoCheesePizza extends Pizza {
+}
--- src/main/java/com/raychen518/study/designpatterns/creational/factory/examples/pizzas/ok2/_Demo.java
+++ src/main/java/com/raychen518/study/designpatterns/creational/factory/examples/pizzas/ok2/_Demo.java
@@ -8,15 +8,44 @@ package com.raychen518.study.designpatterns.creational.factory.examples.pizzas.o
 public class _Demo {
 
 	public static void main(String[] args) {
-		PizzaStore pizzaStore = new PizzaStore(new PizzaFactory());
+		{
+			PizzaStore pizzaStore = new PizzaStore(new NewYorkPizzaFactory());
 
-		pizzaStore.orderPizza(PizzaStore.PIZZA_TYPE_CHEESE);
-		System.out.println();
+			pizzaStore.orderPizza(PizzaStore.PIZZA_TYPE_CHEESE);
+			System.out.println();
 
-		pizzaStore.orderPizza(PizzaStore.PIZZA_TYPE_PEPPER_ONION);
-		System.out.println();
+			pizzaStore.orderPizza(PizzaStore.PIZZA_TYPE_PEPPER_ONION);
+			System.out.println();
 
-		pizzaStore.orderPizza(PizzaStore.PIZZA_TYPE_VEGGIE);
+			pizzaStore.orderPizza(PizzaStore.PIZZA_TYPE_VEGGIE);
+			System.out.println();
+		}
+
+		{
+			PizzaStore pizzaStore = new PizzaStore(new ChicagoPizzaFactory());
+
+			pizzaStore.orderPizza(PizzaStore.PIZZA_TYPE_CHEESE);
+			System.out.println();
+
+			pizzaStore.orderPizza(PizzaStore.PIZZA_TYPE_PEPPER_ONION);
+			System.out.println();
+
+			pizzaStore.orderPizza(PizzaStore.PIZZA_TYPE_VEGGIE);
+			System.out.println();
+		}
+
+		{
+			PizzaStore pizzaStore = new PizzaStore(new CalifoniaPizzaFactory());
+
+			pizzaStore.orderPizza(PizzaStore.PIZZA_TYPE_CHEESE);
+			System.out.println();
+
+			pizzaStore.orderPizza(PizzaStore.PIZZA_TYPE_PEPPER_ONION);
+			System.out.println();
+
+			pizzaStore.orderPizza(PizzaStore.PIZZA_TYPE_VEGGIE);
+			System.out.println();
+		}
 	}
 
 }
